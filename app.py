7fe310fe0771ff90a82a82fafbba3a1ee8e7500b
manni8436 <emanuelsilva019@gmail.com>
2021-09-28 15:38:10 +0000

    Add debug function to app.py file

--- app.py
+++ app.py
@@ -7,3 +7,8 @@ if os.path.exists("env.py"):
 app = Flask(__name__)
 
 @app.route("/")
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
