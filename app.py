cbbd7a9fc250afbe1da2a7f7c76aec6e64838823
manni8436 <emanuelsilva019@gmail.com>
2021-09-28 15:53:45 +0000

    Add test functionality

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ if os.path.exists("env.py"):
 app = Flask(__name__)
 
 @app.route("/")
+def hello():
+  return "Hellow World.... again"
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
