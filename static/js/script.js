9ed3166d9bb04584dc227705558df6051cd93122
manni8436 <emanuelsilva019@gmail.com>
2021-10-09 12:20:15 +0000

    Delete unneeded JS code

--- static/js/script.js
+++ static/js/script.js
@@ -1,17 +1,3 @@
 $(document).ready(function(){
   $('.sidenav').sidenav();
 });
-
-var addFavourites = 'add to favourites<i class="material-icons right">favorite_border</i>';
-var removeFavourites = 'remove from favourites<i class="material-icons right">favorite</i>';
-let buttons = document.getElementsByClassName("favourites-button");
-
-for (let button of buttons) {
-  button.addEventListener("click", function() {
-    if (button.innerHTML == addFavourites) {
-      button.innerHTML = removeFavourites;
-    } else {
-      button.innerHTML = addFavourites;
-    }
-  }, false);
-}
