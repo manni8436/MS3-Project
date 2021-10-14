a1e19a967a7d88132603fb98ce1c597fffc80af4
manni8436 <emanuelsilva019@gmail.com>
2021-10-14 11:35:57 +0000

    Add JS function for carousel to autoplay

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,22 @@
 $(document).ready(function(){
   $('.sidenav').sidenav();
-  $('.carousel').carousel();
+  $('.carousel.carousel-slider').carousel({
+      numVisible: 1
+  }, {
+      duration: 100
+  }, {
+      fullWidth: true
+  });
+});
+
+let autoplay = true;
+
+setInterval(function () {
+    if (autoplay) $('.carousel.carousel-slider').carousel('next');
+}, 5000);
+
+$('.quotes-panel').hover(function () {
+    autoplay = false;
+}, function () {
+    autoplay = true;
 });
